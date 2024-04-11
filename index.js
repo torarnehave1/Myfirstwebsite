97f5e97d8382a6b45146616050a3e086ae22a66c
torarnehave <torarnehave@gmail.com>
2024-04-11 18:14:49 +0200

    New

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//
+//Write a node express server that listens on port 3000 and responds to a GET request with a message of your choice.    
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+}); 
