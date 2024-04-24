a388523ebd62c670da4ed0eefc1833358077816d
torarnehave <torarnehave@gmail.com>
2024-04-24 12:37:07 +0200

    Update database host configuration in user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
